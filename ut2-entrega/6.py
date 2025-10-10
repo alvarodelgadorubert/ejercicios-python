d6f8e3569792a396c0b08e895890fcf9d3b29312
alvarodelgadorubert <alvdelrub@gmail.com>
2025-10-10 11:27:51 +0200

    metemos comentario para no confundirnos

--- ut2-entrega/6.py
+++ ut2-entrega/6.py
@@ -1,2 +1,2 @@
-cadena = "Esta es una 'cadena' con comillas dobles,\nun salto de línea y \tuna tabulación."
+cadena = "Esta es una 'cadena' con comillas dobles,\nun salto de línea y \tuna tabulación." # El salto de línea se hace con \n y la tabulación con \t"
 print(cadena)
