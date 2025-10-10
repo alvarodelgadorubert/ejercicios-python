7804dadf65edc4230871b0422634daa3a31ac1a6
alvarodelgadorubert <alvdelrub@gmail.com>
2025-10-10 11:24:48 +0200

    modificamos parte del código para que coja las vocales tanto en mayúscula como minúscula y con los dos tipos de acentos

--- ut2-entrega/16.py
+++ ut2-entrega/16.py
@@ -2,7 +2,7 @@ frase = input("Escribe una frase: ")
 
 # Reemplazar vocales con asteriscos
 frase_modificada = frase
-for vocal in 'aeiouAEIOUáéíóúÁÉÍÓÚ':
+for vocal in 'aeiouAEIOUáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙ':
     frase_modificada = frase_modificada.replace(vocal, '*')
 
 print(f"Frase original: {frase}")
