8be0019948462d968d59a930c8338616fc70cadc
Alvaro <alvdelrub@gmail.com>
2025-11-05 13:37:10 +0100

    ejecutamos cambios

--- UT3 - entrega/4.py
+++ UT3 - entrega/4.py
@@ -1,14 +1,11 @@
 import sys
 
-# Comprobamos si se ha pasado un argumento (el hostname)
 if len(sys.argv) < 2:
     print("Debes indicar un hostname como argumento.")
     sys.exit()
 
-# Guardamos el argumento en una variable
 hostname = sys.argv[1]
 
-# Comprobamos las condiciones del ejercicio
 if hostname.startswith("PC-") and len(hostname) >= 7:
     print("VÁLIDO")
 else:
