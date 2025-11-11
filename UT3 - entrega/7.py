7c549fe1bf265fa7fdb45005d8ce5a9a4dca178b
Alvaro <alvdelrub@gmail.com>
2025-11-11 09:07:14 +0100

    reorganizamos todo

--- UT3 - entrega/7.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-hoy = datetime.now()
-dia_semana = hoy.weekday()
-
-if dia_semana == 5 or dia_semana == 6:
-    print("Ventana de mantenimiento")
-else:
-    print("Operación normal")
