cb597ce48ff885ca042c004309d3e1c41a54cd42
Alvaro <alvdelrub@gmail.com>
2025-11-12 11:29:07 +0100

    cambiamos cosas

--- UT3 Parte 1 - Entrega/3.py
+++ UT3 Parte 1 - Entrega/3.py
@@ -8,13 +8,13 @@ print(f"Sistema Operativo: {so}")
 print(f"Versión: {version}")
 print(f"Procesador: {procesador}")
 
-gestor_recomendado = "gestor no definido"
-
 if so == "Windows":
     gestor_recomendado = "winget"
 elif so == "Linux":
     gestor_recomendado = "apt"
-elif so == "macOS Darwin":
+elif so == "macOS (Darwin)":
     gestor_recomendado = "brew"
+else:
+    gestor_recomendado = "gestor no definido"
 
 print(gestor_recomendado)
