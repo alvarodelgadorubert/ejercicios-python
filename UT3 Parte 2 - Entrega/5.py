71df67078acf9f38125a97de947d7e00cc019df3
Alvaro <alvdelrub@gmail.com>
2025-11-26 13:50:01 +0100

    subimos el 5

---
+++ UT3 Parte 2 - Entrega/5.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from datetime import datetime
+
+directorio_actual = Path.cwd()
+
+print("--- Archivos .log y Fecha de Modificación ---")
+
+for elemento in directorio_actual.iterdir():
+    if elemento.is_file() and str(elemento).endswith(".log"):
+        nombre_archivo = elemento.name
+        
+        tiempo_modificacion_timestamp = elemento.stat().st_mtime
+        
+        fecha_modificacion = datetime.fromtimestamp(tiempo_modificacion_timestamp)
+        
+        print(f"Archivo: {nombre_archivo} | Modificado: {fecha_modificacion}")
